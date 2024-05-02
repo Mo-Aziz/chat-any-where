56209f255d221a1ab8cf23155d84421ab0bca8c9
Mo-Aziz <mohamed.aref32@gmail.com>
2024-05-02 04:42:29 -0500

    added new chat button

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { MessageSquarePlusIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 function CreateChatButton() {
+  const router = useRouter();
+
+  // create new chat logic.
+  const createNewChat = async () => {
+    // new chat logic
+    router.push(`/chat/abc`);
+  };
   return (
-    <Button variant={"ghost"}>
+    <Button variant={"ghost"} onClick={createNewChat}>
       <MessageSquarePlusIcon />
     </Button>
   );
